8519776f237be56a992a8836dc6738b5d9c78b55
tchezery <tchezeryribeiro@gmail.com>
2026-07-30 08:10:54 -0300

    fix: toast fix

--- login.php
+++ login.php
@@ -43,6 +43,30 @@
             <button type="submit" class="btn btn-primary w-100">Entrar</button>
         </form>
     </div>
+    <script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+    
+    <?php if(isset($_SESSION['toast_msg'])): ?>
+    <div class="toast-container position-fixed bottom-0 end-0 p-3" style="z-index: 9999;">
+        <div id="liveToast" class="toast align-items-center text-bg-<?php echo $_SESSION['toast_type']; ?> border-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="4000">
+            <div class="d-flex">
+                <div class="toast-body">
+                    <?php echo $_SESSION['toast_msg']; ?>
+                </div>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            var toastEl = document.getElementById('liveToast');
+            var toast = new bootstrap.Toast(toastEl);
+            toast.show();
+        });
+    </script>
+    <?php 
+        unset($_SESSION['toast_msg']); 
+        unset($_SESSION['toast_type']);
+    endif; ?>
 </body>
 
 </html>
